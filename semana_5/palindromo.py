78b182b82cca44f3cb29450abbc4fe6dbff9d3bf
José Luis Escobar Pérez <1719110043@utectulancingo.edu.mx>
2020-06-13 04:07:30 +0000

    semana_5

--- semana_5/palindromo.py
+++ semana_5/palindromo.py
@@ -7,7 +7,7 @@ class RepetirProceso: #creamos una clase para el bucle
     repetir = "S" #creamos la variable a evaluar
     while repetir=="S" or repetir=="s" : #mientras cumpla la condicion, todo lo que esté adentro se cumplirá
       
-      class Cadena(RepetirProceso): #creamos la clase para las cadenas y heredamos la clase base (el bucle). Al ya existir un constructor en la clase base ya no es necesario en a clase hija
+      class Cadena(RepetirProceso): #creamos la clase para las cadenas y heredamos la clase base (el bucle). Al ya existir un constructor en la clase base ya no es necesario en la clase hija
 
         texto = input("Escribe algo:") #pedimos al usuario que escriba algo
         
@@ -15,14 +15,16 @@ class RepetirProceso: #creamos una clase para el bucle
 
         def palindromo(self): #creamos el método requerido
           igual, aux = 0, 0 #igualamos a 0 los contadores
-          for i in reversed(range(0, len(self.cadena))):
-            if self.cadena[i].lower() == self.cadena[aux].lower():
-              igual += 1
-            aux += 1
-          if len(self.cadena) == igual:
-            print ("La cadena es palindromo.")
+
+          for i in reversed (range(0,len(self.cadena))): #el bucle for va a repetir sobre el rango de números, pero sera de forma invertida, usando reversed, usamos len para la longitud de la cadena y todo se almacenará en "i"
+            print(i)
+            if self.cadena[i].lower() == self.cadena[aux].lower(): #evaluar si el carácter ubicado en "i" de la variable cadena es igual al carácter ubicado en el índice "aux" en la variable cadena. 
+              igual += 1#si cumple la condicion almacena el resultado
+            aux += 1#si no la cumple, se almacena aquí
+          if len(self.cadena) == igual: #evalua si la longitud de la cadena es igual al valor que tiene la variable igual
+            print ("La cadena es palíndromo.")#imprime esto si es verdadera
           else:
-            print("La cadena no es palindromo.")
+            print("La cadena no es palíndromo.")#caso contrario imprime lo contrario
 
         def espacios(self):
               espacio = self.texto.count(" ") #la funcion count representa la cantidad de apariciones de una subcadena (en este caso los espacios) dentro de la cadena 
@@ -34,14 +36,14 @@ class RepetirProceso: #creamos una clase para el bucle
             i = self.cadena.count("i")
             o = self.cadena.count("o")
             u = self.cadena.count("u")
-            print("No. vocales en la cadena:",a+e+i+o+u)#hacemos la suma para tener el resultado total
+            print("No. vocales en la cadena:",a+e+i+o+u)#hacemos la suma para obtener el resultado total
 
       objeto = Cadena()  #creamos el objeto que llame a la clase
       objeto.palindromo() #llamamos a los métodos
       objeto.espacios()
       objeto.vocales()
       
-      repetir=input("¿Desea analizar otra cadena s/n?:" ) #preguntamos al usuario si quiere un nuevo analisis, si dice que si se repite el proceso hasta que diga que no
+      repetir=input("¿Desea analizar otra cadena s/n? :") #preguntamos al usuario si quiere un nuevo analisis, si dice que si se repite el proceso hasta que diga que no
 
 objetoBucle= RepetirProceso()  #creamos el objeto que llame a la clase
 objetoBucle.repetir() #llamamos al método para que lo ejecute
